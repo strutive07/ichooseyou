7cb3407301039ad9451ec31fe3f848e707038bcc
strutive07 <strutive07@gmail.com>
2018-03-03 23:13:35 +0900

    라우터 연결

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+var __dirname = '../../client/i-choose-you/'
 router.get('/', function(req, res, next) {
-
-  res.render('index', { title: 'Express' });
+    res.sendFile(__dirname + '/index.html');
 });
 
 module.exports = router;
