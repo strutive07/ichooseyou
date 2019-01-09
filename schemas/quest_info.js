d143c0ab3834114fad9c2e872966c0a7d2133478
Wonjun Jang <strutive07@gmail.com>
2019-01-09 15:48:17 +0900

    Update quest_info.js

--- schemas/quest_info.js
+++ schemas/quest_info.js
@@ -17,6 +17,6 @@ const quest_info = mongoose.Schema({
     //는 quest_info 에 저장
 
     //의뢰인 이름, 전화번호는 선배 db 에서 불러오기. request_person_Id 로 불러옴.
-});
+}, { usePushEach: true });
 
 module.exports = quest_info;
