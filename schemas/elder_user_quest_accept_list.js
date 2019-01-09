4fc01b5424a2a82d344b2d80fccbd20880e5a9cd
Wonjun Jang <strutive07@gmail.com>
2019-01-09 15:47:46 +0900

    Update elder_user_quest_accept_list.js

--- schemas/elder_user_quest_accept_list.js
+++ schemas/elder_user_quest_accept_list.js
@@ -4,6 +4,6 @@ var Schema = mongoose.Schema;
 const elder_user_quest_accept_list = mongoose.Schema({
     auth_id : String,
     accept_user_list : Array
-});
+}, { usePushEach: true });
 
 module.exports = elder_user_quest_accept_list;
